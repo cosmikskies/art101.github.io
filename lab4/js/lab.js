213da52bddb1c67d541bc7cb58c5439782a38ff0
cosmikskies <61715140+cosmikskies@users.noreply.github.com>
2021-10-13 22:07:57 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+// Summary. Inserting user name will result in a random set of results consisting of relationship status and name of partner.
+// Generating a random relationship status. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Xuan Luo & Gabriel Rivera
+// @since  10/13/21
+
+//Generate a Random relationship between you and someone else:
+  //Get user Name
+    //Generate a random relationship status
+    //Generate a random name
+  //Combine all three factors into one sentence.
