fcb73f329626272dfe6979d67ae01897c6e4d6cb
cosmikskies <61715140+cosmikskies@users.noreply.github.com>
2021-10-18 17:56:47 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author: Xuan Luo
+* Date: 10/18/21
+* License: Public Domain
+* Partner: Gabriel Rivera
+*/
+
+make = "toyota";
+model = "camry";
+color = "blue";
+year = "2002";
+ownIt = false;
+
+age = 2021 - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writeln("Ownership:" + ownIt + "<br>");
+document.writeln("Age" + age + "<br>");
